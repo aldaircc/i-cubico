9e5fc3017e7261c6e9432b1c0197a840b764ceb9
aromerosud <romerosud89@gmail.com>
2018-12-12 13:49:25 -0500

    add pages despacho, embalaje, inventario

---
+++ src/pages/embalaje/embalaje.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EmbalajePage } from './embalaje';
+
+@NgModule({
+  declarations: [
+    EmbalajePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EmbalajePage),
+  ],
+})
+export class EmbalajePageModule {}
