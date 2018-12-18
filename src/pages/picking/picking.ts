69a3a5fa287f65dcd931331f269f96a3fdd211d8
aromerosud <romerosud89@gmail.com>
2018-12-18 16:14:04 -0500

    programacion de consulta de ordenes picking y ruta de picking

--- src/pages/picking/picking.ts
+++ src/pages/picking/picking.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PickingServiceProvider } from '../../providers/picking-service/picking-service';
+import { IonicPage, NavController, NavParams, ModalController  } from 'ionic-angular';
+import { RutaPickingPage } from '../picking/ruta-picking/ruta-picking'
+import { IncidenciaPage } from '../incidencia/incidencia';
 
 /**
  * Generated class for the PickingPage page.
@@ -15,11 +18,77 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PickingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  userDetail: any;
+  listOrdenesPicking: any;
+  listAuxOrdenesPicking: any;
+  rowCount: any;
+  vPickingPage: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,    
+    public sPicking: PickingServiceProvider, public modalCtrl: ModalController) {
+    const data = JSON.parse(localStorage.getItem('vUserData'));
+    this.userDetail = data;
+    this.getDataOrdenes();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PickingPage');
+  }
+
+   
+
+  getDataOrdenes(){
+    // this.getOrdenesXUsuario(this.userDetail[0].Usuario, 2);
+    this.getOrdenesXUsuario('Admin', 2);
+  }
+
+  getOrdenesXUsuario(strUsuario, intIdAlmacen){
+    
+    this.sPicking.getOrdenesXUsuario(strUsuario, intIdAlmacen).then((result)=>{
+      debugger;
+      this.listOrdenesPicking = result;
+      this.listAuxOrdenesPicking = this.listOrdenesPicking;
+      this.rowCount = this.listAuxOrdenesPicking.length;
+      if(this.listOrdenesPicking.length > 0){
+        console.log('Datos ordenes picking', this.listOrdenesPicking);
+      }else{
+        alert('No se encontrarón datos.');
+      }
+    }, (err)=>{
+      console.log('E-Ordenes Picking listar', err);
+    });
+  }
+
+  goRutaPickingPage(data){
+
+    debugger;
+    if(data.FlagPausa == true){
+      this.showModalIncidencia(data);
+    }else{
+      
+      this.vPickingPage = {
+        'Id_Tx' : data.Id_Tx,
+        'NumOrden' : data.NumOrden
+      };
+
+      this.navCtrl.push(RutaPickingPage, {
+        data: this.vPickingPage
+      });
+    }
+  }
+
+  showModalIncidencia(data){
+    let obj = { 
+        'Id_Tx' : data.Id_Tx,
+        'NumOrden' : data.NumOrden
+      };
+
+    let modalIncidencia = this.modalCtrl.create(IncidenciaPage, { 'pIncidencia' : obj});
+    modalIncidencia.onDidDismiss(data =>{
+      debugger;
+      console.log("datos", data);
+    });
+    modalIncidencia.present();
   }
 
 }
--- src/pages/picking/ruta-picking/ruta-picking.ts
+++ src/pages/picking/ruta-picking/ruta-picking.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DetallePickingPage } from '../detalle-picking/detalle-picking';
-import {PickingPorProductoPage} from '../picking-por-producto/picking-por-producto';
-import {CierrePickingPage} from '../cierre-picking/cierre-picking';
+import { PickingPorProductoPage } from '../picking-por-producto/picking-por-producto';
+import { CierrePickingPage } from '../cierre-picking/cierre-picking';
+import { PickingServiceProvider } from '../../../providers/picking-service/picking-service';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 
 
@@ -20,7 +22,15 @@ import {CierrePickingPage} from '../cierre-picking/cierre-picking';
 })
 export class RutaPickingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  vPickingPage: any = [];
+  listaTempRutaPicking: any = [];
+  rutaPicking: any = [];
+  posicion:number = 0;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public sPicking: PickingServiceProvider) {
+    this.vPickingPage = navParams.get('data');
+    this.getDataRutaPicking(this.vPickingPage.Id_Tx, 'Admin', 2);
   }
 
   goDetallePickingPage():void{
@@ -35,9 +45,36 @@ export class RutaPickingPage {
     this.navCtrl.push(PickingPorProductoPage);
   }
 
+  getDataRutaPicking(strNroDoc, strUsuario, intIdAlmacen){
+    this.sPicking.getDataRutaPicking(strNroDoc, strUsuario, intIdAlmacen).then((result)=>{
+      debugger;
+      this.listaTempRutaPicking = result;
+      this.rutaPicking = result[0];
+      console.log('detalles', this.rutaPicking);
+      if(this.rutaPicking.length == 0){
+        console.log('No se encontraron detalles', this.rutaPicking);  
+      }
+    },err=>{
+      console.log('E-getDetailXTx',err);
+    });
+  }
+
+  NextRutaPicking(){
+    debugger;
+    this.posicion = this.posicion + 1;
+    if(this.posicion<this.listaTempRutaPicking.length){
+      this.rutaPicking = this.listaTempRutaPicking[this.posicion];
+    }
+    
+    
+
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad RutaPickingPage');
   }
+
+
 
 }
 
