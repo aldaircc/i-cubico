fe841771b7f8f3d4cba82c7965a99bbb317bfd7e
Loque#369 <aldair.soft@hotmail.com>
2018-12-26 17:19:56 -0500

    Actualización de captura de datos en login

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -1,17 +1,20 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
+import { GlobalServiceProvider } from '../global-service/global-service';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
-  apiUrl = '';
-  constructor(public http : Http) {
-    //Loque#369Dev
+  headers = new Headers();
+
+  constructor(public http : Http, public sGlobal: GlobalServiceProvider) {
+    this.headers.append('Accept', 'application/json');
+    this.headers.append('Content-Type', 'application/json');
   }
 
   getUsers(credenciales){
     return new Promise(resolve=>{
-       this.http.get(this.apiUrl + 'ValidarUsuario',{ params: credenciales})
+       this.http.get(this.sGlobal.usuario + 'ValidarUsuario',{ params: credenciales})
        .map(res=>res.json())
        .subscribe(data=>{
          resolve(data);
@@ -26,7 +29,7 @@ export class AuthService {
 
     return new Promise(resolve=>{
     
-      this.http.get(this.apiUrl + 'ListarCentrosXUsuario',{ params: userInfo})
+      this.http.get(this.sGlobal.usuario + 'ListarCentrosXUsuario',{ params: userInfo})
       .map(res=>res.json())
       .subscribe(data=>{
         resolve(data);
@@ -39,7 +42,7 @@ export class AuthService {
   getWarehouse(parametros){
     return new Promise(resolve=>{
     
-      this.http.get(this.apiUrl + 'ListarAlmacenesXUsuario',{ params: parametros})
+      this.http.get(this.sGlobal.usuario + 'ListarAlmacenesXUsuario',{ params: parametros})
       .map(res=>res.json())
       .subscribe(data=>{
         resolve(data);
@@ -52,10 +55,10 @@ export class AuthService {
 
   postData(data) {
     return new Promise((resolve, reject) => {
-      let headers = new Headers();
-      headers.append('Content-Type', 'application/json');
+      // let headers = new Headers();
+      // headers.append('Content-Type', 'application/json');
 
-      this.http.post(this.apiUrl+ 'ValidarUsuarioAndroid', JSON.stringify(data), {headers:headers})
+      this.http.post(this.sGlobal.usuario + 'ValidarUsuarioAndroid', JSON.stringify(data), {headers:this.headers})
         .map(res=>res.json())
         .subscribe(res => {
           resolve(res.json());
--- src/providers/global-service/global-service.ts
+++ src/providers/global-service/global-service.ts
@@ -9,8 +9,30 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class GlobalServiceProvider {
+
+  Id_Almacen : number = 1;
+  Id_Impresora : number = 0;
+  nombreImpresora : string;
+  nomberEmpresa : string;
+  userName : string = "";
+  url : string = "http://172.16.32.15:8085/SGAA_WCF/";
+  urlPrint : string = "http://172.16.32.3/SGAA_WCF_PRINTV2/";
+  usuario: string;
+  produccion: string;
+  tablaEst : string;
+  recepcion : string;
+  UMService : string;
+  impresoraService : string;
+  vUserData: any;
   
   constructor(public http: HttpClient) {
-
+    this.userName = "acosetito";
+    this.nomberEmpresa = "TESLA S.A";
+    this.usuario = this.url + 'UsuarioService.svc/rest/';
+    this.produccion = this.url + 'ProduccionService.svc/rest/';
+    this.tablaEst = this.url + 'TablasEstaticasService.svc/rest/';
+    this.recepcion = this.url + 'RecepcionService.svc/rest/';
+    this.UMService = this.url + 'UnidadMedidaService.svc/rest/';
+    this.impresoraService = this.urlPrint + "Impresiones.svc/rest/";
   }
 }
