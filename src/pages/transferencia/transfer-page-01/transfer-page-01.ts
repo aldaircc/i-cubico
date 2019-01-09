b5d8e295389e666f094535a4e4f60641a75ceb3f
Loque#369 <aldair.soft@hotmail.com>
2019-01-09 17:52:21 -0500

    Actualización en modulo de transferencia

--- src/pages/transferencia/transfer-page-01/transfer-page-01.ts
+++ src/pages/transferencia/transfer-page-01/transfer-page-01.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { GlobalServiceProvider } from '../../../providers/global-service/global-service';
 import { PickingServiceProvider } from '../../../providers/picking-service/picking-service';
+import { TransferPage_02Page } from '../transfer-page-02/transfer-page-02';
 
 /**
  * Generated class for the TransferPage_01Page page.
@@ -19,6 +20,8 @@ export class TransferPage_01Page {
 
   listTransf : any;
   rowCount: number = 0;
+  countConfirm: number = 0;
+  countProcess: number = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, 
     public sGlobal: GlobalServiceProvider, public sPicking: PickingServiceProvider,
@@ -29,11 +32,23 @@ export class TransferPage_01Page {
   listarTransferenciaSubAlmacenXUsuario(strUsuario, intIdAlmacen){
     this.sPicking.listarTransferenciaSubAlmacenXUsuario("ADMIN", intIdAlmacen).then(result=>{
       this.listTransf = result;
+      this.countConfirm = this.listTransf.reduce((acc, cur) => cur.Id_Estado === 3 ? ++acc : acc, 0);
+      this.countProcess = this.listTransf.reduce((acc, cur) => cur.Id_Estado === 1 ? ++acc : acc, 0);
     });
   }
 
   goToTransferPage02(data){
     console.log('Data selected', data);
+    let parameter = {
+      'Id_Tx' : data.Id_Tx,
+      'FechaDocumento' : data.FechaDocumento,
+      'Cuenta' : data.Cuenta,
+      'Id_SubAlmacenOrigen' : data.Id_SubAlmacenOrigen,
+      'Id_SubAlmacenDestino' : data.Id_SubAlmacenDestino
+    };
+
+    this.navCtrl.push(TransferPage_02Page, { vParameter: parameter });
+    
     // if (smartGridTx.SelectedCell.RowIndex == -1)
     // {
     //     Resco.Controls.MessageBox.MessageBoxEx.Show(" Seleccione una transacción ", "Picking",
