ca9d9441c62f2222f785f867a8dfa47699bfa507
Loque#369 <aldair.soft@hotmail.com>
2019-01-14 10:52:34 -0500

    actualización de escenario 5 y 6 en transferencia

--- src/pages/transferencia/transfer-page-05/transfer-page-05.ts
+++ src/pages/transferencia/transfer-page-05/transfer-page-05.ts
@@ -22,59 +22,67 @@ export class TransferPage_05Page {
   listUAsTrans: any;
   listAxUATrans: any;
   bolSinUbi: boolean = false;
+  rowCount : number = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public sPicking: PickingServiceProvider, public sGlobal: GlobalServiceProvider) {
-    debugger;
     this.vParameter = this.navParams.get('vParameter');
     this.listarUAsTransferidasXSubAlmacen(this.vParameter.Id_Tx, this.vParameter.Id_Producto, this.vParameter.Lote, this.vParameter.Id_Ubicacion);
   }
 
-  ionViewDidLoad() {
-    
-  }
+  ionViewDidLoad() {}
 
   listarUAsTransferidasXSubAlmacen(strIdTx,intId_Producto, strLote, intId_Ubicacion): void{
-    debugger;
     this.sPicking.listarUAsTransferidasXSubAlmacen(strIdTx, intId_Producto, strLote, intId_Ubicacion).then(result=>{
-      debugger;
       this.listUAsTrans = result;
-      this.listAxUATrans = this.listUAsTrans;
+      this.filterUATransfer(this.bolSinUbi);
     });
   }
 
   checkboxClicked($event) {
-    debugger;
-    this.listAxUATrans = (this.bolSinUbi == true) ? this.listUAsTrans.filter(el => el.Id_UbicacionDestino === null): this.listUAsTrans.filter(el=>el.Id_UbicacionDestino != null);
-    console.log('new value' + this.bolSinUbi);
+    this.filterUATransfer(this.bolSinUbi);
+  }
 
-    // this.booksByStoreID = this.books.filter(
-    //   book => book.store_id === this.store.id);
-
-    // btnChangeUbi.Visible = chkWithOutUbication.Checked;
-    // lstAuxUAsTransfer = (chkWithOutUbication.Checked == true) ?
-    //     lstUAsTransferidas.Where(x => x.Id_UbicacionDestino == null).ToList()
-    //     : lstUAsTransferidas;
-
-    // smgListUAs.DataSource = lstAuxUAsTransfer;
+  filterUATransfer(bolUbi): void{
+    this.listAxUATrans = (bolUbi == true) ? this.listUAsTrans.filter(el => el.Id_UbicacionDestino === null): this.listUAsTrans.filter(el=>el.Id_UbicacionDestino != null);
+    this.rowCount = this.listAxUATrans.length;
   }
 
   goToTransferPage06(): void{
-    debugger;
-    if(this.listUAsTrans.length > 0){
-      let parameter = {};
-      this.navCtrl.push(TransferPage_06Page, { 'vParameter' : parameter });
-  //               manejoPaneles(6);
-  //               lblCodProduct06.Text = strCodProduct.ToString();
-  //               lblProductName06.Text = producto;
-  //               lblCantPalletUbiDest.Text = lstAuxUAsTransfer.Sum(x => x.Cantidad).ToString();
-  //               btnTransferSubAl.Enabled = false;
+    // Cantidad: 2196
+    // CantidadOperacion: 170
+    // CantidadPedida: 170
+    // Cod_Producto: "120502A2602001"
+    // Columna: "04"
+    // Fila: "B"
+    // Id_Producto: 129
+    // Id_SubAlmacen: 2
+    // Id_Tx: "A20190110001"
+    // Id_UM: 13
+    // Id_Ubicacion: 43
+    // Item: 1
+    // Lote: "10603677"
+    // Nivel: "01"
+    // Posicion: "01"
+    // Producto: "PURINOR SUSPENSION x 50 mL M.M."
+    // Saldo: 0
+    // Sector: "SECTOR PT 1"
+    // UM: "UNIDAD"
+    // Ubicacion: "SECTOR PT 1 F: B  - N: 1 - C: 4 - P: 1"
+    // Ubicacion_2: " F: B  - N: 1 - C: 4 - P: 1"
 
-  //               txtIdUbication.Text = string.Empty;
-  //               txtColUbiDest.Text = string.Empty;
-  //               txtRowUbiDest.Text = string.Empty;
-  //               txtLevelUbiDest.Text = string.Empty;
-  //               txtPosUbiDest.Text = string.Empty;
+    if(this.listUAsTrans.length > 0){
+      let parameter = {
+        'Id_Tx': this.vParameter.Id_Tx,
+        'Id_Producto': this.vParameter.Id_Producto,
+        'Lote': this.vParameter.Lote,
+        'Id_Ubicacion': this.vParameter.Id_Ubicacion,
+        'Id_SubAlmacen': this.vParameter.Id_SubAlmacen,
+        'Cod_Producto': this.vParameter.Cod_Producto,
+        'Producto': this.vParameter.Producto,
+        'CantidadPallet': this.listAxUATrans.reduce(function(prev, cur){ return prev + cur.Cantidad }, 0)
+      };
+      this.navCtrl.push(TransferPage_06Page, { 'vParameter' : parameter });
     }else{
       alert('No existen UAs para reubicar.');
       return;
