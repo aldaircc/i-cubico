3c15fb52619777a4967ff2af9173344774d211e4
Loque#369 <aldair.soft@hotmail.com>
2019-01-21 17:37:45 -0500

    Nuevos metodo para Inventario

--- src/providers/inventario-service/inventario-service.ts
+++ src/providers/inventario-service/inventario-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import { GlobalServiceProvider } from '../global-service/global-service';
 
 /*
@@ -69,6 +69,88 @@ export class InventarioServiceProvider {
         result(data);
       },err=>{
         console.log('E-listarPerchasXUsuarioInventario', err);
+      })
+    });
+  }
+
+  iniTerInvXPercha(strIdInventario, intIdSector, strFila, strUsuarioInventariador, strUsuario, intIdRF, intTipo){
+    let parameter = {
+      'strIdInventario' : strIdInventario, 
+      'intIdSector' : intIdSector, 
+      'strFila' : strFila, 
+      'strUsuarioInventariador' : strUsuarioInventariador, 
+      'strUsuario' : strUsuario, 
+      'intIdRF' : intIdRF, 
+      'intTipo' : intTipo
+    };
+
+    return new Promise((result,reject)=>{
+      this.http.post(this.sGlobal.inventarioService + 'IniTerInvXPercha/strIdInventario/intIdSector/strFila/strUsuarioInventariador/strUsuario/intIdRF/intTipo', JSON.stringify(parameter), {headers:this.headers })
+      .map(res=>res.json())
+      .subscribe(data=>{
+        result(data);
+      },err=>{
+        console.log('E-initTerInvXPercha', err);
+      })
+    });
+  }
+
+  iniTerInvXProducto(strIdInventario, intIdProducto, strLote, strUsuarioInventariador, strUsuario, intIdRF, intTipo){
+    let parameter = {
+      'strIdInventario' : strIdInventario, 
+      'intIdProducto' : intIdProducto, 
+      'strLote' : strLote, 
+      'strUsuarioInventariador' : strUsuarioInventariador, 
+      'strUsuario' : strUsuario, 
+      'intIdRF' : intIdRF, 
+      'intTipo': intTipo
+    };
+
+    return new Promise((result, reject)=>{
+      this.http.post(this.sGlobal.inventarioService + 'IniTerInvXProducto?strIdInventario/intIdProducto/strLote/strUsuarioInventariador/strUsuario/intIdRF/intTipo', JSON.stringify(parameter), {headers: this.headers})
+      .map(res=>res.json())
+      .subscribe(data=>{
+        result(data);
+      },err=>{
+        console.log('E-iniTerInvXProducto', err);
+      })
+    });
+  }
+
+  listarUbicacionesSugeridasXProducto(intIdProducto, strLote, intIdAlmacen){
+    let parameter = {
+      'intIdProducto': intIdProducto,
+      'strLote': strLote,
+      'intIdAlmacen': intIdAlmacen
+    };
+
+    return new Promise(result=>{
+      this.http.get(this.sGlobal.inventarioService + 'ListarUbicacionesSugeridasXProducto', {params: parameter})
+      .map(res=>res.json())
+      .subscribe(data=>{
+        result(data);
+      },err=>{
+        console.log('E-listarUbicacionesSugeridasXProducto', err);
+      })
+    });
+  }
+
+  validarUbicacionInventario(CodBarraUbi, intIdAlmacen, intIdSector, strFila, intTipo){
+    let parameter = {
+      'CodBarraUbi': CodBarraUbi, 
+      'intIdAlmacen': intIdAlmacen,
+      'intIdSector': intIdSector, 
+      'strFila': strFila, 
+      'intTipo': intTipo
+    };
+
+    return new Promise((result, reject)=>{
+      this.http.post(this.sGlobal.inventarioService + 'CodBarraUbi/intIdAlmacen/intIdSector/strFila/intTipo', JSON.stringify(parameter), {headers: this.headers})
+      .map(res=>res.json())
+      .subscribe(data=>{
+        result(data);
+      },err=>{
+        console.log('E-validarUbicacionInventario', err);
       })
     });
   }
