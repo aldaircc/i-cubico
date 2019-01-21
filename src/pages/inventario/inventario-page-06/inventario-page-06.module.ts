b316215496718aa3c71da15def9ca363ff236fff
Loque#369 <aldair.soft@hotmail.com>
2019-01-21 17:40:08 -0500

    Escenario 06 de inventario

---
+++ src/pages/inventario/inventario-page-06/inventario-page-06.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { InventarioPage_06Page } from './inventario-page-06';
+
+@NgModule({
+  declarations: [
+    InventarioPage_06Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(InventarioPage_06Page),
+  ],
+})
+export class InventarioPage_06PageModule {}
