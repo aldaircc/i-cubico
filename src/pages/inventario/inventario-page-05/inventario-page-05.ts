7b122c1730610794ccb52a0ec500e39fe0983cbc
Loque#369 <aldair.soft@hotmail.com>
2019-01-21 17:39:45 -0500

    Escenario 05 inventari

---
+++ src/pages/inventario/inventario-page-05/inventario-page-05.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the InventarioPage_05Page page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-inventario-page-05',
+  templateUrl: 'inventario-page-05.html',
+})
+export class InventarioPage_05Page {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad InventarioPage_05Page');
+  }
+
+}
