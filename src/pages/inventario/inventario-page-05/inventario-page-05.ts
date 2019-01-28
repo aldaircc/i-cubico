f4bc4005f330f235af199e77021446b8f3206b07
Loque#369 <aldair.soft@hotmail.com>
2019-01-28 17:48:05 -0500

    Actualización escenario 05 - inventario

--- src/pages/inventario/inventario-page-05/inventario-page-05.ts
+++ src/pages/inventario/inventario-page-05/inventario-page-05.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { InventarioServiceProvider } from '../../../providers/inventario-service/inventario-service';
+import { GlobalServiceProvider } from '../../../providers/global-service/global-service';
 
 /**
  * Generated class for the InventarioPage_05Page page.
@@ -15,11 +17,63 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class InventarioPage_05Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  vParameter: any;
+  listDetail: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
+    public sInve: InventarioServiceProvider, public sGlobal: GlobalServiceProvider) {
+    this.vParameter = this.navParams.get('vParameter');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad InventarioPage_05Page');
+  listarUAsXUbicacionInventario(strIdInventario, strCodBarraUbi): void{
+    this.sInve.listarUAsXUbicacionInventario(strIdInventario, strCodBarraUbi).then(result=>{
+      debugger;
+      this.listDetail = result;
+    });
   }
 
+  ionViewWillEnter(){
+    this.listarUAsXUbicacionInventario(this.vParameter.Id_Inventario, 
+      (this.vParameter.TipoInventario == 'CICLICO') ? this.vParameter.Id_Producto : this.vParameter.Cod_Ubicacion);
+  }
+
+  deleterItem(obj): void{
+   
+    let alert = this.alertCtrl.create({
+      title: 'Confirmar eliminación',
+      message: '¿Está seguro de eliminar el registro?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            
+          }
+        },
+        {
+          text: 'Si',
+          handler: () => {
+            if(this.vParameter.TipoInventario == 'GENERAL'){
+              this.eliminarUAInventario('A20190200001', '00000000000001');//this.vParameter.Id_Inventario, obj.UA_CodBarra);
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  eliminarUAInventario(strIdInventario, strUA): void{
+    this.sInve.eliminarUAInventario(strIdInventario, strUA).then(result=>{
+      let res: any = result;
+      debugger;
+      if(res.errNumber == 0){
+        alert(res.message);
+        this.listarUAsXUbicacionInventario(this.vParameter.Id_Inventario, 
+          (this.vParameter.TipoInventario == 'CICLICO') ? this.vParameter.Id_Producto : this.vParameter.Cod_Ubicacion);
+      }else{
+        alert(res.message);
+      }
+    });
+  }
 }
