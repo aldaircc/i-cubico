2e9b3c9df19bb020c9327c6ae5b5fe82ac95d57f
Loque#369 <aldair.soft@hotmail.com>
2019-01-31 09:42:34 -0500

    Actualización del escenario 06 de inventario

--- src/pages/inventario/inventario-page-06/inventario-page-06.ts
+++ src/pages/inventario/inventario-page-06/inventario-page-06.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { InventarioServiceProvider } from '../../../providers/inventario-service/inventario-service';
+import { GlobalServiceProvider } from '../../../providers/global-service/global-service';
+import { InventarioPage_04Page } from '../inventario-page-04/inventario-page-04';
 
 /**
  * Generated class for the InventarioPage_06Page page.
@@ -16,14 +19,61 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class InventarioPage_06Page {
 
   vParameter: any;
+  listUbicacion: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    debugger;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public sInve: InventarioServiceProvider, public sGlobal: GlobalServiceProvider) {
     this.vParameter = this.navParams.get('vParameter');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad InventarioPage_06Page');
+  listarUbicacionesSugeridasXProducto(intIdProducto, strLote, intIdAlmacen): void{
+    this.sInve.listarUbicacionesSugeridasXProducto(intIdProducto, strLote, intIdAlmacen).then(result=>{
+      this.listUbicacion = result;
+    });
+  }
+  
+  ionViewWillEnter(){
+    this.listarUbicacionesSugeridasXProducto(this.vParameter.Id_Producto, this.vParameter.Lote, this.sGlobal.Id_Almacen);
   }
 
+  goToInventPage04(obj): void{
+    debugger;
+    var parameter: any;
+
+    parameter = (this.vParameter.TipoInventario == 'GENERAL') ? 
+      {
+        'Fila': this.vParameter.Fila,
+        'Id_Estado': this.vParameter.Id_Estado,
+        'Id_Inventario': this.vParameter.Id_Inventario,
+        'Id_Sector': this.vParameter.Id_Sector,
+        'Sector': this.vParameter.Sector,
+        'UsuarioAsignado': this.vParameter.UsuarioAsignado,
+        'UsuarioInventariador': this.vParameter.UsuarioInventariador,
+        'TipoInventario': this.vParameter.TipoInventario
+      }
+    :
+      {
+        'Id_Inventario' : this.vParameter.Id_Inventario,
+        'Id_Estado' : this.vParameter.Id_Estado,
+        'UsuarioInventariador' : this.vParameter.UsuarioInventariador,
+        'UsuarioAsignado' : this.vParameter.UsuarioAsignado,
+        'Id_Producto' : this.vParameter.Id_Producto,
+        'Codigo' : this.vParameter.Codigo,
+        'Producto' : this.vParameter.Producto,
+        'Lote' : this.vParameter.Lote,
+        'TipoInventario': this.vParameter.TipoInventario,
+        
+        'CodigoBarra': obj.CodigoBarra,
+        'Columna': obj.Columna,
+        'Fila': obj.Fila,
+        'FlagTransito': obj.FlagTransito,
+        'Id_Sector': obj.Id_Sector,
+        'Id_Ubicacion': obj.Id_Ubicacion,
+        'Nivel': obj.Nivel,
+        'Posicion': obj.Posicion,
+        'Sector': obj.Sector
+      };
+
+    this.navCtrl.push(InventarioPage_04Page, { 'vParameter': parameter });
+  }
 }
