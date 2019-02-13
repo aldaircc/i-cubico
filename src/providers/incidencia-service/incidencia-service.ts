59443e231d6418f0c90be89afa3e72d11cfd7c13
Loque#369 <aldair.soft@hotmail.com>
2019-02-13 17:56:00 -0500

    Actualización de incidencia .ts

--- src/providers/incidencia-service/incidencia-service.ts
+++ src/providers/incidencia-service/incidencia-service.ts
@@ -21,21 +21,11 @@ export class IncidenciaServiceProvider {
     console.log('Hello IncidenciaServiceProvider Provider');
     this.headers.append('Accept', 'application/json');
     this.headers.append('Content-Type', 'application/json');
-    //this.headers.append('dataType', 'json');
   }
 
   listarCausalesXModulo(intIdCliente, intIdModulo){
-    /**
-          RecepcionService.svc
-    @GET("ListarCausalesXModulo")
-    Call<ArrayList<Causal>> listarCausalesXModulo(
-            @Query("intIdCliente") Integer intIdCliente,
-            @Query("intIdModulo") Integer intIdModulo
-    ); 
-  **/
     let parameter : any;
     parameter = {'intIdCliente': intIdCliente, 'intIdModulo': intIdModulo};
-    debugger;
     return new Promise(resolve=>{
       this.http.get(this.apiUrl + this.recepcion + 'ListarCausalesXModulo', {params: parameter})
       .map(res=>res.json())
@@ -48,14 +38,6 @@ export class IncidenciaServiceProvider {
   }
 
   registrarControlOP(strIdOP, intIdLineaMAQ, intIdCausal, strUsuario, strObservacion, bFlagPausa){
-    /** 
-        ProduccionService.svc
-            @Headers({"Accept:application/json", "Content-Type:application/json"})
-        @POST("RegistrarControlOP/strIdOP/intIdLineaMAQ/intIdCausal/strUsuario/strObservacion/bFlagPausa")
-        Call<Mensaje> registrarControlOP(
-                @Body JsonObject param
-                ); 
-    **/
    let parameter = {'strIdOP': strIdOP, 'intIdLineaMAQ' : intIdLineaMAQ, 'intIdCausal' : intIdCausal, 'strUsuario' : strUsuario, 'strObservacion' : strObservacion, 'bFlagPausa' : bFlagPausa };
 
    return new Promise((resolve, reject)=>{
@@ -70,20 +52,6 @@ export class IncidenciaServiceProvider {
   }
 
   registrarControl(id_Tx, id_Causal, usuario, id_TerminalRF, observacion, flagPausa){
-  /**
-      TablasEstaticasService.svc
-      @Headers({"Accept: application/json", "Content-Type: application/json"})
-      @POST("RegistrarControl")
-      Call<Mensaje> registrarControl(
-              @Query("Id_Tx") String strId_Tx,
-              @Query("Id_Causal") Integer intId_Causal,
-              @Query("Usuario") String strUsuario,
-              @Query("Id_TerminalRF") Integer intId_TerminalRF,
-              @Query("Observacion") String strObservacion,
-              @Query("FlagPausa") Boolean bolFlagPausa
-      );
-  **/
-
     let urlSearchParams = new URLSearchParams();
     urlSearchParams.append('Id_Tx', id_Tx);
     urlSearchParams.append('Id_Causal', id_Causal);
