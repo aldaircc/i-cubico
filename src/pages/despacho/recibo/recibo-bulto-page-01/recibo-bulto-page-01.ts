49fa4d8e0db1ebb1509101db70360cf084bc6e14
Loque#369 <aldair.soft@hotmail.com>
2019-02-14 10:06:53 -0500

    Se agregó nuevo escenario para despacho

---
+++ src/pages/despacho/recibo/recibo-bulto-page-01/recibo-bulto-page-01.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ReciboBultoPage_01Page page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-recibo-bulto-page-01',
+  templateUrl: 'recibo-bulto-page-01.html',
+})
+export class ReciboBultoPage_01Page {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+}
