edbd01391cd3f5e80da37f3f6c1243087025abd8
aromerosud <romerosud89@gmail.com>
2019-02-21 09:55:39 -0500

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -96,26 +96,6 @@ import { EmbalajePage_06Page } from '../pages/embalaje/embalaje-page-06/embalaje
 import { EmbalajePage_07Page } from '../pages/embalaje/embalaje-page-07/embalaje-page-07';//YUS
 import { EmbalajePage_08Page } from '../pages/embalaje/embalaje-page-08/embalaje-page-08';//YUS
 import { EmbalajePage_09Page } from '../pages/embalaje/embalaje-page-09/embalaje-page-09';//YUS
-import {TransitoPage} from '../pages/almacenaje/transito/transito'; //aromero
-import {PalletsTransitoPage} from '../pages/almacenaje/pallets-transito/pallets-transito'; //aromero
-import {DetallePalletUaPage} from '../pages/almacenaje/detalle-pallet-ua/detalle-pallet-ua'; //aromero
-import {OtraUbicacionPage} from '../pages/almacenaje/otra-ubicacion/otra-ubicacion'; //aromero
-import {AlmacenajePalletUaPage} from '../pages/almacenaje/almacenaje-pallet-ua/almacenaje-pallet-ua'; //aromero
-import {ReubicacionPage} from '../pages/almacenaje/reubicacion/reubicacion'; //aromero
-import {ReubicacionDestinoPage} from '../pages/almacenaje/reubicacion-destino/reubicacion-destino'; //aromero
-import {ReabastecimientoAlmacenajePage} from '../pages/almacenaje/reabastecimiento-almacenaje/reabastecimiento-almacenaje'; //aromero
-import {ReabastecimientoPickingPage} from '../pages/almacenaje/reabastecimiento-picking/reabastecimiento-picking'; //aromero
-import {ListaPalletUaPage} from '../pages/almacenaje/lista-pallet-ua/lista-pallet-ua'; //aromero
-import {UbicacionDestinoPage} from '../pages/almacenaje/ubicacion-destino/ubicacion-destino'; //aromero
-import {ConsultarUbicacionPage} from '../pages/almacenaje/consultar-ubicacion/consultar-ubicacion'; //aromero
-import {DetalleProductoPage} from '../pages/almacenaje/detalle-producto/detalle-producto'; //aromero
-import {MenuConsultarPage} from '../pages/almacenaje/menu-consultar/menu-consultar'; //aromero
-import {AdministrarUaPage} from '../pages/almacenaje/menu-consultar/administrar-ua/administrar-ua'; //aromero
-import {ReasignarUaPage} from '../pages/almacenaje/menu-consultar/reasignar-ua/reasignar-ua'; //aromero
-import {ReubicarUaPage} from '../pages/almacenaje/menu-consultar/reubicar-ua/reubicar-ua'; //aromero
-import {ParticionarUaPage} from '../pages/almacenaje/menu-consultar/particionar-ua/particionar-ua'; //aromero
-import {ConsultarPalletPage} from '../pages/almacenaje/menu-consultar/consultar-pallet/consultar-pallet'; //aromero
-import {ConsultarEanPage} from '../pages/almacenaje/menu-consultar/consultar-ean/consultar-ean'; //aromero
 import { SelectAllDirective } from '../directives/select-all/select-all';
 import { AlmacenajeServiceProvider } from '../providers/almacenaje-service/almacenaje-service';
 import { InventarioServiceProvider } from '../providers/inventario-service/inventario-service';
