805a5b3cdd1131efb6651382fcee4e13f15907cc
aromerosud <romerosud89@gmail.com>
2019-03-14 10:58:05 -0500

    actualizacion de detalle producto y reabastecimiento

--- src/pages/picking/reabastecimiento/reabastecimiento.ts
+++ src/pages/picking/reabastecimiento/reabastecimiento.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, PopoverController, AlertController } from 'ionic-angular';
+import { IonicPage, App, ModalController, NavController, NavParams, PopoverController, AlertController } from 'ionic-angular';
 import { PopoverRutaPickingPage } from '../../picking/popover/popover-ruta-picking/popover-ruta-picking'
 import { AlmacenajeServiceProvider } from '../../../providers/almacenaje-service/almacenaje-service';
 import { GlobalServiceProvider } from '../../../providers/global-service/global-service';
+import { IncidenciaPage } from '../../incidencia/incidencia';
+import { AdministrarUaPage } from '../../almacenaje/menu-consultar/administrar-ua/administrar-ua'
+import { ConsultarUbicacionPage } from '../../almacenaje/consultar-ubicacion/consultar-ubicacion'
+import { MainMenuPage } from '../../main-menu/main-menu'
+import { HomePage } from '../../home/home';
 /**
  * Generated class for the ReabastecimientoPage page.
  *
@@ -20,7 +25,7 @@ export class ReabastecimientoPage {
   vPickingXProducto: any = [];
   TextObservacion: string = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
+  constructor(public app: App,  public modalCtrl: ModalController, public navCtrl: NavController, public navParams: NavParams,
     public popoverCtrl: PopoverController, public sAlmacenaje: AlmacenajeServiceProvider,
     public sGlobal: GlobalServiceProvider, public alertCtrl: AlertController) {
     this.vPickingXProducto = navParams.get('data');
@@ -47,11 +52,62 @@ export class ReabastecimientoPage {
     this.navCtrl.pop();
   }
 
+  showModalIncidencia2(){ //data
+    debugger;
+    let modalIncidencia = this.modalCtrl.create(IncidenciaPage); //{ 'pIncidencia' : obj});
+    modalIncidencia.onDidDismiss(data =>{
+      if(data.response == 200){
+        this.navCtrl.pop();
+      }
+    });
+    modalIncidencia.present();
+  }
+
+  goAdministrarUaPage() {
+    this.vPickingXProducto = {
+      'page': 6
+    };
+    this.navCtrl.push(AdministrarUaPage, {
+      data: this.vPickingXProducto
+    });
+  }
+
+  goConsultarUbicacionPage() {
+    this.navCtrl.push(ConsultarUbicacionPage);
+  }
+
+  goMenu() {
+    debugger;
+    this.navCtrl.push(MainMenuPage);
+  }
+
   presentPopover(ev) {
     let popover = this.popoverCtrl.create(PopoverRutaPickingPage, {
+      // contentEle: this.content.nativeElement,
+      // textEle: this.text.nativeElement
     });
     popover.present({
       ev: ev
+    });
+
+    popover.onDidDismiss(popoverData => {
+      if (popoverData == 1) {
+        this.showModalIncidencia2();
+      } else if (popoverData == 2) {
+        debugger;
+        this.goAdministrarUaPage();
+      } else if (popoverData == 3) {
+        debugger;
+        this.goConsultarUbicacionPage();
+      } else if (popoverData == 4) {
+        debugger;
+        this.goMenu();
+      } else if (popoverData == 5) {
+        debugger;
+        this.navCtrl.pop();
+        var nav = this.app.getRootNav();
+        nav.setRoot(HomePage);
+      }
     });
   }
 
