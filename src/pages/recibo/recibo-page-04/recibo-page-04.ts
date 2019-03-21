502f3218352e400076ac29b9a364db84382dbc89
Loque#369 <aldair.soft@hotmail.com>
2019-03-21 09:35:46 -0500

    Atención de observaciones en modulo Recibo - escenario 04

--- src/pages/recibo/recibo-page-04/recibo-page-04.ts
+++ src/pages/recibo/recibo-page-04/recibo-page-04.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController, PopoverController, ModalController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController, PopoverController, ModalController, LoadingController, VirtualScroll } from 'ionic-angular';
 import { ReciboServiceProvider } from '../../../providers/recibo-service/recibo-service';
 import { GlobalServiceProvider } from '../../../providers/global-service/global-service';
 import { ReciboPage_05Page } from '../recibo-page-05/recibo-page-05';
@@ -24,85 +24,125 @@ export class ReciboPage_04Page {
   listBulto : any = [];
   rowCount : number = 0;
   callBackReciboPage04: any;
-
+  wasDeleted: boolean = false;
+  @ViewChild('virtualScroll', { read: VirtualScroll }) virtualScroll: VirtualScroll;
+  
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public alertCtrl: AlertController , public sRecibo: ReciboServiceProvider,
     public sGlobal: GlobalServiceProvider, public popoverCtrl: PopoverController,
-    public modalCtrl: ModalController) {
+    public modalCtrl: ModalController, public loadingCtrl: LoadingController) {
     debugger;
     this.vReciboPage03 = navParams.get('dataPage03');
     console.log('datos de pagina 03', this.vReciboPage03);
     this.callBackReciboPage04 = navParams.get('callBackReciboPage04');
-    this.listarUAXProductoTx(this.vReciboPage03.Id_Tx, this.vReciboPage03.Id_Articulo, this.vReciboPage03.Item);
+    //this.listarUAXProductoTx(this.vReciboPage03.Id_Tx, this.vReciboPage03.Id_Articulo, this.vReciboPage03.Item);
   }
 
-  ionViewDidLoad() {
-    
+  ionViewWillEnter() {
+    this.listarUAXProductoTx(this.vReciboPage03.Id_Tx, this.vReciboPage03.Id_Articulo, this.vReciboPage03.Item);
   }
 
   ionViewWillLeave(){
     debugger;
-    let param = this.vReciboPage03.Saldo;
+    let param = { 'saldo': this.vReciboPage03.Saldo, 'wasDeleted': this.wasDeleted };
     this.callBackReciboPage04(param).then(()=>{
       //this.navCtrl.pop();
     });
   }
 
+  loading: any;
+
+  presentLoadingDefault() {
+    debugger;
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+  
+    this.loading.present();
+  
+    // setTimeout(() => {
+    //   this.loading.dismiss();
+    // }, 5000);
+  }
+
   listarUAXProductoTx(strIdTx, intIdProducto, intItem){
+    this.listBulto = [];
+    this.presentLoadingDefault();
+    this.virtualScroll.renderVirtual(true);
+
     this.sRecibo.listarUAXProductoTx(strIdTx, intIdProducto, intItem).then(result=>{
-      this.listBulto = result;
+      let res: any = []; 
+      res = result;
+      this.listBulto = res;
       debugger;
       this.rowCount = this.listBulto.length;
+      this.virtualScroll.renderVirtual(true);
+      this.loading.dismiss();
     });
   }
 
-  deleteItem(data){
+  presentConfirmDialog(title, message): Promise<boolean> {
+    return new Promise((resolve, reject) => {
 
-    let alert = this.alertCtrl.create({
-      title: 'Confirmar eliminación',
-      message: '¿Esta seguro de eliminar el registro?',
-      buttons: [
+      const confirm = this.alertCtrl.create({
+        title: title,
+        message: message,
+        buttons: [{
+          text: 'Si',
+          handler: () => {
+            resolve(true);
+          },
+        },
         {
           text: 'No',
           role: 'cancel',
           handler: () => {
-            
-          }
-        },
-        {
-          text: 'Si',
-          handler: () => {
-            debugger;
-            let currentDate = new Date().toISOString();
-            let objUA = {
-              'UA_CodBarra': data.UA_CodBarra,
-              'Id_Tx': data.Id_Tx,
-              'Id_Producto': data.Id_Producto,
-              'LoteLab': data.LoteLab,
-              'FechaEmision' : "/Date("+ Date.parse(currentDate) +")/",
-              'FechaVencimiento' : "/Date("+ Date.parse(currentDate) +")/",
-              'FechaIngreso' : "/Date("+ Date.parse(currentDate) +")/",
-              'Cantidad' : data.Cantidad,
-              'Saldo' : data.Cantidad,
-              'CantidadAveriada' : data.CantidadAveriada,
-              'Id_TerminalRF' : 1,
-              'Item' : this.vReciboPage03.Item,
-              'UsuarioRegistro' : this.sGlobal.userName,
-              'Id_Almacen' : this.sGlobal.Id_Almacen,
-              'FlagAnulado' : true,
-              'Observacion' : ''
-            };
-            
-            this.evaluateDelete(objUA);
+            resolve(false);
           }
         }
       ]
+      });
+      confirm.present();
+    })
+  }
+
+  deleteItem(data){
+
+    this.presentConfirmDialog('Confirmar eliminación', '¿Esta seguro de eliminar el registro?').then(rpta=>{
+
+      if(rpta == true){
+        debugger;
+        let currentDate = new Date().toISOString();
+        let objUA = {
+          'UA_CodBarra': data.UA_CodBarra,
+          'Id_Tx': data.Id_Tx,
+          'Id_Producto': data.Id_Producto,
+          'LoteLab': data.LoteLab,
+          'FechaEmision' : "/Date("+ Date.parse(currentDate) +")/",
+          'FechaVencimiento' : "/Date("+ Date.parse(currentDate) +")/",
+          'FechaIngreso' : "/Date("+ Date.parse(currentDate) +")/",
+          'Cantidad' : data.Cantidad,
+          'Saldo' : data.Cantidad,
+          'CantidadAveriada' : data.CantidadAveriada,
+          'Id_TerminalRF' : 1,
+          'Item' : this.vReciboPage03.Item,
+          'UsuarioRegistro' : this.sGlobal.userName,
+          'Id_Almacen' : this.sGlobal.Id_Almacen,
+          'FlagAnulado' : true,
+          'Observacion' : ''
+        };
+        
+        this.evaluateDelete(objUA);
+      }else{
+        return;
+      }
+
     });
-    alert.present();
   }
 
   evaluateDelete(objUA){
     debugger;
+    this.wasDeleted = true;
     if(this.vReciboPage03.Id_TipoMovimiento == 0){
       alert('Esta transacción no tiene tipo de movimiento');
       return;
