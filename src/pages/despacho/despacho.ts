f7bf994ab36348f63d41bab728611eb180d454e2
Loque#369 <aldair.soft@hotmail.com>
2019-03-21 09:33:20 -0500

    Atención de observaciones en modulo despacho

--- src/pages/despacho/despacho.ts
+++ src/pages/despacho/despacho.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, PopoverController, ModalController, App } from 'ionic-angular';
 import { EmbarquePage_01Page } from './embarque/embarque-page-01/embarque-page-01';
 import { ReciboBultoPage_01Page } from './recibo/recibo-bulto-page-01/recibo-bulto-page-01';
+import { PopoverReciboComponent } from '../../components/popover-recibo/popover-recibo';
+import { HomePage } from '../home/home';
+import { ImpresoraPage } from '../impresora/impresora';
 
 /**
  * Generated class for the DespachoPage page.
@@ -17,7 +20,30 @@ import { ReciboBultoPage_01Page } from './recibo/recibo-bulto-page-01/recibo-bul
 })
 export class DespachoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public app: App, public navCtrl: NavController, public navParams: NavParams, 
+    public popoverCtrl: PopoverController, public modalCtrl: ModalController) {
+  }
+
+  presentPopover(myEvent){
+    let popover = this.popoverCtrl.create(PopoverReciboComponent, {'page' : 0 });
+    popover.present({
+      ev: myEvent
+    });
+
+    popover.onDidDismiss(popoverData =>{
+     if(popoverData == 3){
+        this.showModalImpresora();
+      }else if(popoverData == 4){
+        this.navCtrl.pop();
+        var nav = this.app.getRootNav();
+        nav.setRoot(HomePage);
+      }
+    });
+  }
+
+  showModalImpresora(){
+    let modalIncidencia = this.modalCtrl.create(ImpresoraPage);
+    modalIncidencia.present();
   }
 
   goToEmbarPage01(){
