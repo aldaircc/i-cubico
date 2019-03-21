149dcef83309378ef8350dbfa402e79221bbf3b4
Loque#369 <aldair.soft@hotmail.com>
2019-03-21 09:33:51 -0500

    Atención de observaciones en modulo despacho - escenario 02

--- src/pages/despacho/embarque/embarque-page-02/embarque-page-02.ts
+++ src/pages/despacho/embarque/embarque-page-02/embarque-page-02.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, PopoverController, App } from 'ionic-angular';
 import { GlobalServiceProvider } from '../../../../providers/global-service/global-service';
 import { DespachoServiceProvider } from '../../../../providers/despacho-service/despacho-service';
 import { EmbarquePage_03Page } from '../embarque-page-03/embarque-page-03';
-
+import { PopoverReciboComponent } from '../../../../components/popover-recibo/popover-recibo';
+import { HomePage } from '../../../home/home';
 /**
  * Generated class for the EmbarquePage_02Page page.
  *
@@ -22,10 +23,25 @@ export class EmbarquePage_02Page {
   totalSubBultos: number = 0;
   totalSubBultosLeido: number = 0;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public sGlobal: GlobalServiceProvider,
+  constructor(public app: App, public popoverCtrl: PopoverController, public modalCtrl:ModalController, public navCtrl: NavController, public navParams: NavParams, public sGlobal: GlobalServiceProvider,
     public sDesp: DespachoServiceProvider) {
     this.vParameter = this.navParams.get('vParameter');
     this.listarSubBultosLeidos(this.vParameter.Id_Tra, 2);
+  }
+
+presentPopover(myEvent){
+    let popover = this.popoverCtrl.create(PopoverReciboComponent, {'page' : 1});
+    popover.present({
+      ev: myEvent
+    });
+
+    popover.onDidDismiss(popoverData =>{
+      if(popoverData == 4){
+        this.navCtrl.pop();
+        var nav = this.app.getRootNav();
+        nav.setRoot(HomePage);
+      }
+    });
   }
 
   listarSubBultosLeidos(strTransaccion, tipo){
